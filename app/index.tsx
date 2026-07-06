81f6d88afc50d14728ffccd510ee8d2a5155cc90
jzchinn <jzchinn@gmail.com>
2026-07-05 19:21:45 -0700

    implement the onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-background">
-      <Text className="h1 text-center color-lingua-purple">Duolingo Clone</Text>
+      <Text className="h1 text-center color-lingua-purple">triolingo</Text>
       <Text className="h2 text-center">A great app</Text>
+      <Link href="/onboarding" className="mt-6 body-lg text-lingua-purple">
+        Go to onboarding
+      </Link>
     </View>
   );
 }
